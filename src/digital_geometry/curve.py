7ef5254e12649fa681f82c59c48d3672448df104
pants64DS <pants64DS@users.noreply.github.com>
2024-11-25 20:13:53 +0200

    Limit the discrete curve at its first control point

--- src/digital_geometry/curve.py
+++ src/digital_geometry/curve.py
@@ -1,15 +1,15 @@
 from util import IntVec
 
-def _parabola_encloses(point, a, b, c, d, e, offset_times_two):
-	x, y = 2 * point + offset_times_two
+def parabola_encloses(a, b, c, d, e, u, offset_times_two):
+	x, y = 2 * u + offset_times_two
 	f = a * x - b * y
 
 	return f * f < (2 * (d * y - c * x) - 2 * e) * 2 * e
 
-def leaning_curve_encloses_point(curve, point):
-	p0 = curve.p0 - curve.p1
-	p2 = curve.p2 - curve.p1
-	point = IntVec(point) - curve.p1
+def point_in_discrete_curve(u, p0, p1, p2):
+	p0 -= p1
+	p2 -= p1
+	u  -= p1
 
 	# if p0.x == -p2.x or p0.y == -p2.y:
 	# 	return False
@@ -19,12 +19,18 @@ def leaning_curve_encloses_point(curve, point):
 	if p0.x < -p2.x:
 		p0.x = -p0.x
 		p2.x = -p2.x
-		point.x = -point.x
+		u.x = -u.x
 
 	if p0.y < -p2.y:
 		p0.y = -p0.y
 		p2.y = -p2.y
-		point.y = -point.y
+		u.y = -u.y
+
+	# Mirror the curve along the diagonal to make it turn counterclockwise
+	if p0.x * p2.y > p0.y * p2.x:
+		p0.x, p0.y = p0.y, p0.x
+		p2.x, p2.y = p2.y, p2.x
+		u.x, u.y = u.y, u.x
 
 	a = p0.y + p2.y
 	b = p0.x + p2.x
@@ -32,31 +38,48 @@ def leaning_curve_encloses_point(curve, point):
 	d = p0.x - p2.x
 	e = p0.x * p2.y - p2.x * p0.y
 
-	in_P0 = _parabola_encloses(point, a, b, c, d, e, IntVec(1, -1))
-	in_P1 = _parabola_encloses(point, a, b, c, d, e, IntVec(-1, 1))
+	u_in_P0 = parabola_encloses(a, b, c, d, e, u, IntVec(1, -1))
+	u_in_P1 = parabola_encloses(a, b, c, d, e, u, IntVec(-1, 1))
+	u_in_P2 = parabola_encloses(a, b, c, d, e, u, IntVec(-1, -1))
+	u_in_P3 = parabola_encloses(a, b, c, d, e, u, IntVec(1, 1))
 
-	if in_P0 and in_P1 and _parabola_encloses(point, a, b, c, d, e, IntVec(-1, -1)):
+	if u_in_P0 and u_in_P1 and u_in_P2:
 		return
 
 	# Min x and min y
-	if b * (2 * point.x + 1) <= 2 * p0.x * p2.x \
-	or a * (2 * point.y + 1) <= 2 * p0.y * p2.y:
+	if b * (2 * u.x + 1) <= 2 * p0.x * p2.x \
+	or a * (2 * u.y + 1) <= 2 * p0.y * p2.y:
 		return False
 
 	# Top left parabola
-	if b * (b * (2 * point.y - 1) - a * (2 * point.x + 1)) \
-	>= 2 * d * e and not in_P0:
+	if not u_in_P0 and b * (b * (2 * u.y - 1) - a * (2 * u.x + 1)) >= 2 * d * e:
 		return False
 
 	# Bottom right parabola
-	if a * (b * (2 * point.y + 1) - a * (2 * point.x - 1)) \
-	<= 2 * c * e and not in_P1:
+	if not u_in_P1 and a * (b * (2 * u.y + 1) - a * (2 * u.x - 1)) <= 2 * c * e:
 		return False
 
 	# Bottom left parabola
-	if a * (2 * point.x + 1) + b * (2 * point.y + 1) \
-	<= 2 * (p0.x * p2.y + p2.x * p0.y) \
-	and not _parabola_encloses(point, a, b, c, d, e, IntVec(1, 1)):
+	if not u_in_P3 and a * (2 * u.x + 1) + b * (2 * u.y + 1) <= 2 * (p0.x * p2.y + p2.x * p0.y):
+		return False
+
+	# Check if point u is between the endpoints
+
+	# If p0 is in the first quadrant
+	if p0.x >= 0 and p0.y >= 0:
+		if u_in_P0 and not u_in_P1 \
+		and (u.y > p0.y or (u.y == p0.y and u.x > p0.x)):
+			return False
+
+	# If p0 is in the third quadrant
+	elif p0.x <= 0 and p0.y <= 0:
+		if u_in_P0 and not u_in_P1 \
+		or u.x < p0.x or (u.x == p0.x and u.y < p0.y):
+			return False
+
+	# If p0 is in the second quadrant
+	elif u.x < p0.x or (u.x == p0.x and u.y > p0.y) \
+	or (u_in_P0 and (u_in_P2 or u.y > p0.y)):
 		return False
 
 	return True
@@ -68,19 +91,14 @@ class Curve:
 		self.p2 = IntVec(p2)
 
 	def __contains__(self, point):
-		return leaning_curve_encloses_point(self, point)
-
-	def in_bounding_box(self, point):
-		min_x, _, max_x = sorted([self.p0.x, self.p1.x, self.p2.x])
-		min_y, _, max_y = sorted([self.p0.y, self.p1.y, self.p2.y])
-
-		return min_x <= point.x <= max_x and min_y <= point.y <= max_y
+		return point_in_discrete_curve(IntVec(point), self.p0, self.p1, self.p2)
 
 	def search(self, point, visited):
 		if point in visited: return
 		visited.add(point)
 
-		if point not in self or not self.in_bounding_box(point): return
+		if point not in self or abs(point.x) > 50 or abs(point.y) > 50:
+			return
 		yield point
 
 		yield from self.search(IntVec(point.x + 1, point.y), visited)
